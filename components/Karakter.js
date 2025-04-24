9a2bdb610354060176d99802f143ed51a3f0f1ad
anggitPM17 <167511000+anggitPM17@users.noreply.github.com>
2025-04-24 21:52:55 +0700

    Update Karakter.js

--- components/Karakter.js
+++ components/Karakter.js
@@ -1,1 +1,15 @@
+// components/Karakter.js
+import Image from 'next/image';
+
+const Karakter = ({ nama, gambar, status }) => {
+  return (
+    <div className="text-center">
+      <h2 className="text-2xl font-bold mb-4">{nama}</h2>
+      <Image src={gambar} alt={nama} width={200} height={200} />
+      <p className="mt-2">Status: {status}</p>
+    </div>
+  );
+};
+
+export default Karakter;
 
