d200f8014724fe715fd9740d98dabad0c3d6dd13
anggitPM17 <167511000+anggitPM17@users.noreply.github.com>
2025-04-24 22:35:02 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        jawa: ['"Javanese Text"', 'serif'],
+        lora: ['Lora', 'serif'],
+      },
+      backgroundImage: {
+        'gunung': "url('/background.jpg')",
+      },
+    },
+  },
+  plugins: [],
+}
 
